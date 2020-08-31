782701765ca82ab317d32d5bd64b7de0ae2843f1
ajpicard913 <Contact@saucedesigns.io>
2020-08-31 11:26:37 -0400

    Initial Commit

---
+++ Shared/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  Shared
+//
+//  Created by AJ Picard on 8/31/20.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
